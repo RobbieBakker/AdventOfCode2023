861bb320b1a688112457f121cf46397d8ac50e6d
Robin Bakker <robinb@live.nl>
2023-12-03 19:24:35 +0100

    Day 2 finished, Day 3 work in progress
    DONE: dag 2
    TODO: dag 3 werkt nog niet

---
+++ 2 dec/2 dec/Program.cs
@@ -0,0 +1,94 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text.RegularExpressions;
+using static System.Net.Mime.MediaTypeNames;
+
+
+string path = "C:\\Users\\Robin\\Documents\\GitHub\\AdventOfCode2023\\2 dec\\input.txt";
+
+//part1(path);
+part2(path);
+
+
+
+static void part1(string path)
+{
+    int sum = 0;
+    using (StreamReader sr = new StreamReader(path))
+    {
+        string line;
+        while ((line = sr.ReadLine()) != null)
+        {
+            //Console.WriteLine("line:" + line);
+            if (checkColor(line, "blue", 14) == true && checkColor(line, "green", 13) == true && checkColor(line, "red", 12) == true)
+            {
+                string pattern = @"\bGame\s(\d+)\b";
+                var matches = Regex.Match(line, pattern);
+                sum += int.Parse(matches.Groups[1].Value);
+            }
+
+        }
+    }
+    Console.WriteLine(sum);
+}
+
+static Boolean checkColor(string line, string color, int maxValue)
+{
+    string pattern = @"\b(\d+)\s" + color + "\\b";
+    var matches = Regex.Matches(line, pattern);
+
+    foreach (Match match in matches)
+    {
+        if (int.Parse(match.Groups[1].Value) > maxValue)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+static void part2(string path)
+{
+    int sum = 0;
+    using (StreamReader sr = new StreamReader(path))
+    {
+        string line;
+        while ((line = sr.ReadLine()) != null)
+        {
+            int maxBlue = 0;
+            int maxGreen = 0;
+            int maxRed = 0;
+
+
+            //Console.WriteLine("line:" + line);
+            // check maxvalue of blue
+            maxBlue = getMinimumNeededCubes(line, "blue");
+            maxGreen = getMinimumNeededCubes(line, "green");
+            maxRed = getMinimumNeededCubes(line, "red");
+
+            int power = maxBlue * maxGreen * maxRed;
+
+            sum += power;
+            //string pattern = @"\bGame\s(\d+)\b";
+
+        }
+    }
+    Console.WriteLine(sum);
+}
+
+static int getMinimumNeededCubes(string line, string color)
+{
+    int maxValue = 0;
+
+    string pattern = @"\b(\d+)\s"+color+"\\b";
+
+    var matches = Regex.Matches(line, pattern);
+    foreach (Match match in matches)
+    {
+        if (int.Parse(match.Groups[1].Value) > maxValue)
+        {
+            maxValue = int.Parse(match.Groups[1].Value);
+        }
+    }
+    //Console.WriteLine("min " + color + " needed: " + maxValue);
+    return maxValue;
+}
---
+++ 3 dec/3 dec/Program.cs
@@ -0,0 +1,80 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text.RegularExpressions;
+
+string path = "C:\\Users\\Robin\\Documents\\GitHub\\AdventOfCode2023\\3 dec\\input2.txt";
+
+part1(path);
+
+static void part1(string path)
+{
+    char[,] array = buildArray(path);
+    List<int> parts = new List<int>();
+    parts.Add(0);
+    
+    for (int i = 0; i < array.GetLength(0); i++)
+    {//each line
+
+        for(int j = 0; j < array.GetLength(1); j++)
+        {//each character
+            string number = "";
+            char[] specialChars = { '@', '#', '$', '%', '&', '*', '-', '=', '+', '/'};
+            if (array[i, j].ToString().Any(char.IsDigit))
+            {//if there is a number
+                
+                if(
+                    specialChars.Contains(array[i-1, j-1]) || specialChars.Contains(array[i-1, j]) || specialChars.Contains(array[i-1, j+1]) || 
+                    specialChars.Contains(array[i, j-1]) || specialChars.Contains(array[i, j+1]) || 
+                    specialChars.Contains(array[i+1, j-1]) || specialChars.Contains(array[i+1, j]) || specialChars.Contains(array[i+1, j + 1]))
+                {// if aangrenzend bevat speciaal teken ga naar begin van getal en add ze toe tot number string
+
+                    int k = 0;
+                    while (array[i, j + k].ToString().Any(char.IsDigit))
+                    {
+                        k--;
+                    }
+                    k += 1; // index kwam 1 te laag uit
+                    while (array[i, j + k].ToString().Any(char.IsDigit))
+                    {
+                        number += array[i, j + k];
+                        k++;
+                    }
+                    if(!number.Equals(""))
+                    {
+                        if (!parts.Last().Equals(int.Parse(number)))
+                        {
+                            parts.Add(int.Parse(number));
+                            //Console.WriteLine(number);
+                        }
+                        number = "";
+                    }
+                }
+            }
+        }
+    }
+
+    Console.WriteLine(parts.Sum());
+}
+
+static char[,] buildArray(string path)
+{
+    using (StreamReader sr = new StreamReader(path))
+    {
+        char[,] array = new char[142, 143];
+        string line;
+
+        int lineNumber = 0;
+        // looping through the lines
+        while ((line = sr.ReadLine()) != null)
+        {
+            // looping through the characters
+            for (int i = 0; i < line.Length; i++)
+            {
+                array[lineNumber, i] = line[i];
+            }
+
+            lineNumber++;
+        }
+        return array;
+    }
+}
